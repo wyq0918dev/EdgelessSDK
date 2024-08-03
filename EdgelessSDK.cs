55e567f85433f9108bf854ce0fb64ca57823bec3
wyq0918dev <wyq0918dev@gmail.com>
2024-08-04 01:01:37 +0800

    添加项目文件。

---
+++ EdgelessSDK.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EdgelessSDK
+{
+    public partial class EdgelessSDK : Form
+    {
+        public EdgelessSDK()
+        {
+            InitializeComponent();
+        }
+    }
+
+    internal static class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            new Launcher().LaunchGUI();
+        }
+    }
+   
+    internal class Launcher
+    {
+        [STAThread]
+        public void LaunchGUI()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new EdgelessSDK());
+        }
+    }
+}
